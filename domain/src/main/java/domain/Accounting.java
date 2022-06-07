da4c8eab289d23c18e24890d6a679d43257c0576
Sergey Shishkin <sergey@shishkin.org>
2022-06-07 17:22:20 +0200

    Make sure accounting stays consistent by implementing Iterable only

--- domain/src/main/java/domain/Accounting.java
+++ domain/src/main/java/domain/Accounting.java
@@ -3,14 +3,19 @@ package domain;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class Accounting extends ArrayList<Travel> {
+public class Accounting implements Iterable<Travel> {
+
+    final List<Travel> accounting = new ArrayList<>();
+
     public void enterTravel(LocalDateTime start, LocalDateTime end, SystemClock clock) throws OnlyOneSimultaneousTravelAllowed, TravelExpenseIsTooLate, TravelEndMustOccurAfterEnd {
         travelEndMustOccurAfterStart(start, end);
         travelMustBeEnteredBeforeJan10FollowingYear(end, clock);
         travelsMustNotOverlap(start, end);
 
-        this.add(new Travel(start, end));
+        accounting.add(new Travel(start, end));
     }
 
     private void travelEndMustOccurAfterStart(LocalDateTime start, LocalDateTime end) throws TravelEndMustOccurAfterEnd {
@@ -21,7 +26,7 @@ public class Accounting extends ArrayList<Travel> {
 
 
     public void travelsMustNotOverlap(LocalDateTime start, LocalDateTime end) throws OnlyOneSimultaneousTravelAllowed {
-        if (this.stream().anyMatch((travel) -> travel.start().compareTo(start) <= 0 && travel.end().compareTo(end) >= 0)) {
+        if (accounting.stream().anyMatch((travel) -> travel.start().compareTo(start) <= 0 && travel.end().compareTo(end) >= 0)) {
             throw new OnlyOneSimultaneousTravelAllowed();
         }
     }
@@ -31,5 +36,10 @@ public class Accounting extends ArrayList<Travel> {
         if (end.getYear() == now.getYear() - 1 && now.getMonthValue() >= Month.JANUARY.getValue() && now.getDayOfMonth() > 10) {
             throw new TravelExpenseIsTooLate();
         }
+    }
+
+    @Override
+    public Iterator<Travel> iterator() {
+        return accounting.iterator();
     }
 }
