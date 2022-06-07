6074a22042bb70149ff66f20dbd75bd9ab9e9a05
Sergey Shishkin <sergey@shishkin.org>
2022-06-07 17:07:16 +0200

    Add time-based constraints for recording

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ so that I get my travel expenses reimbursed.
 * Travel end date must occur after travel start date
 * Accounting accepts only one simultaneous travel
 
+## Past travels
+
+Travels can only be entered until January 10th of the following year,
+so that accounting can close financial year reporting.
--- domain/src/test/java/domain/EntryTest.java
+++ domain/src/test/java/domain/EntryTest.java
@@ -5,15 +5,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class EntryTest {
 
     Accounting accounting = new Accounting();
+    SystemClock defaultClock = SystemClock.Default;
 
     @BeforeEach
-    void setup(){
+    void setup() {
         accounting = new Accounting();
     }
 
@@ -24,7 +28,7 @@ public class EntryTest {
         var destination = "any";
         var reason = "any";
 
-        enter(start, end, destination, reason);
+        enter(start, end, destination, reason, defaultClock);
     }
 
     @Test
@@ -34,7 +38,7 @@ public class EntryTest {
         var destination = "any";
         var reason = "any";
 
-        assertThrows(TravelEndMustOccurBeforeStart.class, () -> enter(start, end, destination, reason));
+        assertThrows(TravelEndMustOccurBeforeStart.class, () -> enter(start, end, destination, reason, defaultClock));
     }
 
     @Test
@@ -44,15 +48,56 @@ public class EntryTest {
         var destination = "any";
         var reason = "any";
 
-        enter(start, end, destination, reason);
-        assertThrows(OnlyOneSimultaneousTravelAllowed.class, () -> enter(start, end, destination, reason));
+        enter(start, end, destination, reason, defaultClock);
+        assertThrows(OnlyOneSimultaneousTravelAllowed.class, () -> enter(start, end, destination, reason, defaultClock));
     }
 
-    public void enter(LocalDateTime start, LocalDateTime end, String destination, String reason) throws TravelEndMustOccurBeforeStart, OnlyOneSimultaneousTravelAllowed {
+    @Test
+    void Should_reject_2021_travel_after_Jan_11_2022() throws Exception {
+        var start = LocalDateTime.MIN;
+        var end = LocalDateTime.of(2021, Month.DECEMBER, 31, 0, 0);
+        var destination = "any";
+        var reason = "any";
+        SystemClock jan11Clock = () -> LocalDateTime.of(2022, Month.JANUARY, 11, 0, 0);
+
+        assertThrows(TravelExpenseIsTooLate.class, () -> enter(start, end, destination, reason, jan11Clock));
+    }
+
+    @Test
+    void Should_accept_2021_travel_before_Jan_10_2022() throws Exception {
+        var start = LocalDateTime.MIN;
+        var end = LocalDateTime.of(2021, Month.DECEMBER, 31, 0, 0);
+        var destination = "any";
+        var reason = "any";
+
+        var jan10Clock = mock(SystemClock.class);
+        when(jan10Clock.now()).thenReturn(LocalDateTime.of(2022, Month.JANUARY, 10, 0, 0));
+
+        enter(start, end, destination, reason, jan10Clock);
+    }
+
+    public void enter(LocalDateTime start, LocalDateTime end, String destination, String reason, SystemClock clock) throws TravelEndMustOccurBeforeStart, OnlyOneSimultaneousTravelAllowed, TravelExpenseIsTooLate {
         if (end.compareTo(start) < 0) {
             throw new TravelEndMustOccurBeforeStart();
         }
 
+        var now = clock.now();
+        if (end.getYear() == now.getYear() - 1
+                && now.getMonthValue() >= Month.JANUARY.getValue()
+                && now.getDayOfMonth() > 10) {
+            throw new TravelExpenseIsTooLate();
+        }
+
         accounting.enterTravel(start, end);
+    }
+
+    @FunctionalInterface
+    interface SystemClock {
+        LocalDateTime now();
+
+        SystemClock Default = LocalDateTime::now;
+    }
+
+    public static class TravelExpenseIsTooLate extends Exception {
     }
 }
