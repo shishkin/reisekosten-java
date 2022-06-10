8ccaeed92c0e6504c569fd432b4c8901ea024c21
Sergey Shishkin <sergey@shishkin.org>
2022-06-10 22:18:28 +0200

    Replace if/return with map

--- domain/src/main/java/domain/Travel.java
+++ domain/src/main/java/domain/Travel.java
@@ -2,17 +2,24 @@ package domain;
 
 import java.math.BigDecimal;
 import java.time.Duration;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.function.Predicate;
 
 public record Travel(TravelExpenseForm form) {
+
+    static final List<Entry<Predicate<Duration>, BigDecimal>> durationAllowances = List.of(
+            new SimpleEntry<>(d -> d.compareTo(Duration.ofHours(24)) >= 0, BigDecimal.valueOf(24)),
+            new SimpleEntry<>(d -> d.compareTo(Duration.ofHours(12)) >= 0, BigDecimal.valueOf(12)),
+            new SimpleEntry<>(d -> d.compareTo(Duration.ofHours(8)) >= 0, BigDecimal.valueOf(6))
+    );
+
     BigDecimal allowance() {
-        if (duration().compareTo(Duration.ofHours(24)) >= 0) {
-            return BigDecimal.valueOf(24);
-        }
-        if (duration().compareTo(Duration.ofHours(12)) >= 0) {
-            return BigDecimal.valueOf(12);
-        }
-        if (duration().compareTo(Duration.ofHours(8)) >= 0) {
-            return BigDecimal.valueOf(8);
+        for (var entry : durationAllowances) {
+            if (entry.getKey().test(duration())) {
+                return entry.getValue();
+            }
         }
         return BigDecimal.valueOf(0);
     }
